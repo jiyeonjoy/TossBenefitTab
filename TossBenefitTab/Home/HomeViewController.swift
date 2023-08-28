9b9c27bf87d91d2db85589cf60bbde4362e5ffd0
최지연 <choi02647@naver.com>
2023-08-28 13:39:41 +0900

    added HomeViewController

---
+++ TossBenefitTab/Home/HomeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  HomeViewController.swift
+//  TossBenefitTab
+//
+//  Created by 최지연/클라이언트 on 2023/08/28.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+}
